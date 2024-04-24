f7f5a716ca5c8f9dff977fbb9d0edf2b69912316
carfdev <jose.carlos@fdez.se>
2024-04-24 19:38:52 +0200

    declare colors for tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'primary': {
+        light: '#28519c',
+        DEFAULT: '#1e3c74',
+        dark: '#14274b',
+      },
+			'secondary': {
+				light: '#85ad71',
+				DEFAULT: '#6b9556',
+			},
+      neutral: "#fdf4a1",
+    }
 	},
 	plugins: [],
 }
